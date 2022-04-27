8a7c7ad5539909aab63778f50ba6fd6d799d7bba
smithan kumar r <smithankumar404@gmail.com>
2022-04-27 10:49:41 +0530

    learn

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,9 +1,14 @@
 // 1. Create an array named numbers and store 5 number values in it
-
+let numbers = [ 14, 26, 24, 18, 17];
 // 2. Calculate the sum of array items and print it to the console using console.log()
-
+for(let num of numbers){
+    let sum =0 ;
+    sum += num ;
+    console.log(sum)
+}
 // 3. Calculate the average of array items and print it to the console using console.log()
-
+let average = numbers.length / numbers.length -1;
+console.log(average)
 // 4. Find the highest number in the array and print it to the console using console.log()
 
 // 5. Find the lowest number in the array and print it to the console using console.log()

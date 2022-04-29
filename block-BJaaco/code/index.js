bfbb38729e2cf35d292ffd50817e0b85fab16d8e
smithan kumar r <smithankumar404@gmail.com>
2022-04-29 11:48:00 +0530

    ..

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,10 +3,11 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
-
+let collection = numbers + userIds;
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
-
+let evenCollection = (collection % 2 == 0);
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let oddCollection = (collection % 2 !== 0);
 
 /*
   @param means parameter
@@ -24,10 +25,9 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
 
-function times() {
-  // Your code
+function times(a, b) {
+  return [b]
 }
-
 // Uncomment the code below and test the output
 
 // console.log(times(5, 'c')); // ['c', 'c', 'c', 'c', 'c']
@@ -48,8 +48,13 @@ function times() {
     revert(['Ryan', 'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function revert() {
-  // your code
+function revert(arr) {
+  for( let i=0; i<arr.length ;i++){
+   var temp = arr.length;
+    temp--;
+    console.log(temp)
+  }
+  return temp;
 }
 
 // Uncomment the code below and test the output
@@ -70,8 +75,10 @@ function revert() {
     clear(['Ryan', null, 0,  'John', 'Bran']); //['Bran', 'John', 'Ryan']
 */
 
-function clear() {
-  // your code
+function clear(arr) {
+  if(arr === true) {
+    return arr;
+  }
 }
 
 // Uncomment the code below and test the output
@@ -93,8 +100,8 @@ function clear() {
     arrayToObj(['Ryan', 'John']); // {0: 'Ryan', 1: 'John'}
 */
 
-function arrayToObj() {
-  // your code
+function arrayToObj(arr) {
+  return  {i ,arr}
 }
 
 // Uncomment the code below and test the output
